5a696e53febb818d52a8869e349674961e867814
Brendan Jackman <brendan.jackman@arm.com>
2017-07-22 21:01:39 +0100

    server: switch indentation to 2 spaces
    This is what the create-react-app boilerplate code uses so it's what I've used
    on the client, so let's use it on the server too.

--- app.js
+++ app.js
@@ -13,30 +13,30 @@ if (!process.env.TWILIO_SOURCE_NUMBER)
 
 console.log('Setting up Twilio client.');
 const twilioClient = new twilio(
-    process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN
+  process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN
 );
 console.log('Twilio client setup done.');
 
 function sendSms(number, body) {
-    twilioClient.messages.create({
-        to: number,
-        // TODO: Using a fixed source number won't scale, because apparently if
-        // too many texts come from a single number, carriers will filter them.
-        // https://www.twilio.com/docs/api/rest/sending-messages-copilot
-        from: process.env.TWILIO_SOURCE_NUMBER,
-        body: body
-    }).then((message) => console.log(message.sid));
+  twilioClient.messages.create({
+    to: number,
+    // TODO: Using a fixed source number won't scale, because apparently if
+    // too many texts come from a single number, carriers will filter them.
+    // https://www.twilio.com/docs/api/rest/sending-messages-copilot
+    from: process.env.TWILIO_SOURCE_NUMBER,
+    body: body
+  }).then((message) => console.log(message.sid));
 }
 
-app.get('/api', (req, res) => {
-    var number = '+447402103030';
-    console.log('request!')
-    // console.log('sending message to ${number}');
-    // sendSms(number, 'Hello world');
-    res.status(200).send('Hello, world!').end();
+app.post('/api', (req, res) => {
+  var number = '+447402103030';
+  console.log('request!')
+  // console.log('sending message to ${number}');
+  // sendSms(number, 'Hello world');
+  res.status(200).send('Hello, world!').end();
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
