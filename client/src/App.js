b7ed1ca7d566f448f26b2d7fe8d8bfd84e95c8a0
Brendan Jackman <brendan.jackman@arm.com>
2017-07-22 18:41:14 +0100

    client: Update submission button with validity of phone number

--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,50 @@ import Phone from 'react-phone-number-input'
 import 'react-phone-number-input/rrui.css';
 import 'react-phone-number-input/style.css';
 
-import {isValidNumber} from 'libphonenumber-js';
+import {isValidNumber, format as formatNumber} from 'libphonenumber-js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      phoneNumber: ''
+      phoneNumber: '',
+      country: 'GB'
     }
   }
 
   handleChange(value) {
     this.setState({phoneNumber: value});
-    console.log(isValidNumber(value));
+  }
+
+  handleCountryChange(value) {
+    this.setState({country: value});
   }
 
   render() {
+    const countryError = this.state.country != 'GB' ?
+          'Sorry, only United Kingdom phone numbers are supported' : ''
+
+    const buttonText = isValidNumber(this.state.phoneNumber) && !countryError ?
+          'Send SMS to ' + formatNumber(this.state.phoneNumber, 'GB', 'International') :
+          'Enter valid number to send SMS'
+
     return (
       <div className="App">
         <div className="Phone-container">
           <Phone
-            country='GB'
+            country={this.state.country}
             placeholder="Enter phone number"
             value={this.state.phoneNumber}
-            onChange={value => this.handleChange(value)} />
+            onChange={value => this.handleChange(value)}
+            onCountryChange={value => this.handleCountryChange(value)}
+          />
         </div>
-        <p className="output">
-          You entered {this.state.phoneNumber}
-        </p>
+        <div className="country-error">
+          <p> {countryError} </p>
+        </div>
+        <button disabled={!isValidNumber(this.state.phoneNumber)}>
+          {buttonText}
+        </button>
       </div>
     );
   }
