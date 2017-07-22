39aeaa9441ca63388f771c8e4e9f2298f657c383
Brendan Jackman <brendan.jackman@arm.com>
2017-07-22 23:48:28 +0100

    client: Add state machine for registration progress

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import 'react-phone-number-input/style.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
@@ -20,12 +21,20 @@ injectTapEventPlugin();
 
 const muiTheme = getMuiTheme();
 
+// Pseudo-enum used for a state machine in the PhoneSubmitter component
+const machineStates = {
+  NEED_NUMBER: 0,               // Waiting for user to give valid number
+  REGISTRATION_REQUESTED: 1,    // Waiting for response from backend
+  REGISTRATION_SUCCESSFUL: 2    // Finished
+};
+
 class PhoneSubmitter extends Component {
   constructor(props) {
     super(props);
     this.state = {
       phoneNumber: '',
-      country: 'GB'
+      country: 'GB',
+      machineState: machineStates.NEED_NUMBER
     }
 
     this.handleChangeNumber = this.handleChangeNumber.bind(this);
@@ -48,7 +57,11 @@ class PhoneSubmitter extends Component {
       headers: {
         "Content-Type": "application/json"
       }
-    }).then((response) => console.log(response));
+    }).then((response) => {
+      console.log(response);
+      this.setState({machineState: machineStates.REGISTRATION_SUCCESSFUL});
+    });
+    this.setState({machineState: machineStates.REGISTRATION_REQUESTED});
   }
 
   render() {
@@ -65,27 +78,46 @@ class PhoneSubmitter extends Component {
           'Send SMS to ' + formatNumber(this.state.phoneNumber, 'GB', 'National') :
           'Enter valid number to send SMS'
 
+    // We render totally different things depending on our state machine.
+    // Either a phone number input form, a loading spinner or a success message.
+    // TODO: Would a proper React approach be to have three separate components?
+    //       Does it matter?
     // NB: the onCountryChange prop below doesn't seem to be documented, but I
     // found it in the source code for the component :/
-
-    return (
-      <div className="PhoneSubmitter">
-        <Phone
-          country={this.state.country}
-          placeholder="Enter phone number"
-          value={this.state.phoneNumber}
-          onChange={value => this.handleChangeNumber(value)}
-          onCountryChange={value => this.handleChangeCountry(value)} />
-        <div className="country-error">
-          <p> {countryError} </p>
+    switch (this.state.machineState) {
+    case machineStates.NEED_NUMBER:
+      return (
+        <div className="PhoneSubmitter">
+          <Phone
+            country={this.state.country}
+            placeholder="Enter phone number"
+            value={this.state.phoneNumber}
+            onChange={value => this.handleChangeNumber(value)}
+            onCountryChange={value => this.handleChangeCountry(value)} />
+          <div className="country-error">
+            <p> {countryError} </p>
+          </div>
+          <RaisedButton disabled={!readyToSend}
+                        value={this.state.phoneNumber}
+                        primary={true}
+                        onTouchTap={this.handleSubmit}
+                        label={buttonText} />
         </div>
-        <RaisedButton disabled={!readyToSend}
-                      value={this.state.phoneNumber}
-                      primary={true}
-                      onTouchTap={this.handleSubmit}
-                      label={buttonText} />
-      </div>
-    );
+      );
+    case machineStates.REGISTRATION_REQUESTED:
+      return (
+        <div className="PhoneSubmitter">
+          <CircularProgress />
+        </div>
+      );
+    case machineStates.REGISTRATION_SUCCESSFUL:
+      return (
+        <div className="PhoneSubmitter">
+          <p>DONE</p>
+        </div>
+      );
+    default: return; // Probably can't be salvaged if we got here..
+    }
   }
 }
 
